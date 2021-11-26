01d0a5aa59f13a84879c6a778eeb55d916b76320
gwansik <rhkstlr00@gmail.com>
2021-11-26 18:01:39 +0900

    list만들기 중간 커밋

--- js/todolist.js
+++ js/todolist.js
@@ -1,3 +1,45 @@
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.querySelector("#todo-list");
+
+let toDos = [];
+const TODOS = "todos";
+
+function saveToDos() {
+    localStorage.setItem(TODOS, JSON.stringify(toDos));
+}
+
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function appendList(todo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = todo;
+    const button = document.createElement("button");
+    button.innerText = "✖︎";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
+    todoList.appendChild(li);
+}
+
+function toDoSubmit(event) {
+    event.preventDefault();
+    const todo = todoInput.value;
+    todoInput.value = "";
+    toDos.push(todo);
+    appendList(todo);
+    saveToDos();
+}
+todoForm.addEventListener("submit", toDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS);
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(appendList);
+}
